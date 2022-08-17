49a0fd607e0e396c47d23b70ac80c1573f7226d5
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-08-17 16:50:44 +0600

    added rasturant app top banner and update responsiveness

--- src/pages/TopBanner.js
+++ src/pages/TopBanner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IoMdAddCircle } from 'react-icons/io';
 import '../styles/TopBanner.css'
 
 const TopBanner = () => {
@@ -9,7 +10,23 @@ const TopBanner = () => {
                     <div className='slide'>
                         <div className='box'>
                             <div className="content">
-                                
+                                <div className='banner-title '>
+                                    <span>DOUBLE</span> 
+                                    <span>MUSHROOM</span>
+                                    <span>BURGER</span>
+                                </div>
+                                <div className='price'>
+                                    <li>Cheese</li>
+                                    <li>Mushroom</li>
+                                    <div></div>
+                                    <span className='flex items-center'>
+                                        <span className='flex flex-col relative'>
+                                            <span><small>only</small></span>
+                                            <span className='price__rs'>Rs. 199</span>
+                                        </span>
+                                        <span className='plus'><IoMdAddCircle/></span>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
