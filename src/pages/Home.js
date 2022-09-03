65b18c244561d95c783052946500a749fca85d98
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-04 02:48:14 +0600

    Added new section why choose us

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import About from './About';
 import TopBanner from './TopBanner';
+import WhyChoose from './WhyChoose';
 
 const Home = () => {
     return (
         <Helmet title="Home">
             <TopBanner/>
             <About/>
+            <WhyChoose/>
         </Helmet>
     );
 };
---
+++ src/pages/WhyChoose.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../styles/WhyChoose.css'
+import burger1 from '../assets/images/Choose Burger1.png'
+import burger2 from '../assets/images/Choose Burger2.png'
+import McLogo from '../assets/images/MC-LOGO-TEXT.png'
+import girl from '../assets/images/Girl-Image.png'
+
+const WhyChoose = () => {
+    return (
+        <div className='choose__container'>
+            <div className='choose__wrapper'>
+                <div className='choose__title'>
+                    <img src={McLogo} alt="" srcset="" />
+                    <h1>Why Choose us?</h1>
+                </div>
+                <div className='choose__content'>
+                    <div className="choose__img">
+                        <img src={burger1} alt="" srcset="" />
+                    </div>
+                    <div className='choose__info'>
+                        <h1>Commitment to quality</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil accusamus suscipit magnam natus! Autem sed maxime, natus sit eveniet rem.</p>
+                    </div>
+                </div>
+                <div className='choose__content'>
+                    <div className='choose__info'>
+                        <h1>What's in you food</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil accusamus suscipit magnam natus! Autem sed maxime, natus sit eveniet rem.</p>
+                    </div>
+                    <div className="choose__img">
+                        <img src={girl} alt="" srcset="" />
+                    </div>
+                </div>
+                <div className='choose__content'>
+                    <div className="choose__img">
+                        <img src={burger2} alt="" srcset="" />
+                    </div>
+                    <div className='choose__info'>
+                        <h1>Commitment to quality</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil accusamus suscipit magnam natus! Autem sed maxime, natus sit eveniet rem.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhyChoose;
