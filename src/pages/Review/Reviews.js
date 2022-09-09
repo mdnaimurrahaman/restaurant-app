2df217cd3c8263d9dc4c862b882e2d384f8a6174
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-09 21:16:06 +0600

    Review section design and swiper js added

--- src/pages/Review/Reviews.js
+++ src/pages/Review/Reviews.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Data from './Data';
-import img from '../../assets/images/Reviews img.jpg'
-import style from './Reviews.module.css'
+import React from "react";
+import Data from "./Data";
+import img from "../../assets/images/Reviews img.jpg";
+import style from "./Reviews.module.css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -10,36 +10,51 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
-
 // import required modules
 import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from "swiper";
 
-
 const Reviews = () => {
-    const reviews = Data;
-    console.log(reviews)
-    return (
-        <div className={style.reviews__container}>
-            <div className={style.review__title}>
-                <p>Clients Review</p>
-                <span>Testimonials</span>
-            </div>
-            <div className={style.reviews__wrapper}>
-                <div className={style.client__img}>
-                    <img src={img} alt="" srcset="" />
-                </div>
-                <Swiper
-        pagination={{
+  const reviews = Data;
+  console.log(reviews);
+  return (
+    <div className={style.reviews__container}>
+      <div className={style.review__title}>
+        <p>Clients Review</p>
+        <span>Testimonials</span>
+      </div>
+      <div className={style.reviews__wrapper}>
+        {/* <div className={style.client__img}>
+          <img src={img} alt="" srcset="" />
+        </div> */}
+        <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+          pagination={{
             dynamicBullets: true,
-          }}  modules={[Pagination, Autoplay]} autoplay={{delay:2200}}  className='reviews mySwiper'>
-                    {
-                        reviews.map(review => <SwiperSlide key={review.id}><p>{review.description}</p><h1>{review.name}</h1></SwiperSlide>)
-                    }
-                </Swiper>
-            </div>
-        </div>
-    );
+          }}
+          modules={[Pagination, Autoplay]} autoplay={{delay:2200}}
+          className="reviews mySwiper"
+        >
+          {reviews.map((review) => (
+            <SwiperSlide key={review.id}>
+              <div className={style.swiper__client__msg}>
+                <p>{review.description}</p>
+              </div>
+              <div className={style.swiper__client__data}>
+                <figure>
+                  <img src={review.cover} alt="client review" srcset="" />
+                </figure>
+                <div className={style.client__data__details}>
+                  <h3>{review.name}</h3>
+                  <p>{review.time}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
