256cee3ce6a51a1966e43ece6247232ded5813ec
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-12 23:28:28 +0600

    Added Resturant app Last bannerr contetn

---
+++ src/pages/BottomBanner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const BottomBanner = () => {
+    return (
+        <section className='last__container'>
+            <div className='last__content'>
+                <div className='last__details'>
+                    <h1>Our High Quality Ingredients</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid minus suscipit optio cumque beatae? Est eos quia nam. Cupiditate, totam.</p>
+                </div>
+                <div className='last__button'>
+                    <button>Discover the Menu</button>
+                    <button>Daily Offers</button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default BottomBanner;
