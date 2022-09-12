b26fc2e2d804318aad63ad347cd1a98898c6b5c7
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-09-13 02:59:20 +0600

    Rasturant app bottom banner desing done

--- src/pages/BottomBanner.js
+++ src/pages/BottomBanner.js
@@ -7,11 +7,11 @@ const BottomBanner = () => {
             <div className='last__content'>
                 <div className='last__details'>
                     <h1>Our High Quality Ingredients</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid minus suscipit optio cumque beatae? Est eos quia nam. Cupiditate, totam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga enim eum ducimus, nostrum aperiam tempore laboriosam eaque iure quidem necessitatibus? Ex temporibus nulla laudantium consequuntur rem recusandae ipsum, possimus fugiat neque corporis consectetur, at nisi.</p>
                 </div>
                 <div className='last__button'>
-                    <button>Discover the Menu</button>
-                    <button>Daily Offers</button>
+                    <button className='banner-button'>Discover the Menu</button>
+                    <button className='banner-button'>Daily Offers</button>
                 </div>
             </div>
         </section>
